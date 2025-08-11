b34785c003d8c20da2ce76f3aeaf72ba8268effb
Ayush Bhandarkar <163156376+Aayushbhandarkar@users.noreply.github.com>
2025-08-11 19:36:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://ai-virtual-assistant-l6fy.onrender.com
+    "https://ai-virtual-assistant-l6fy.onrender.com"
   ],
   credentials: true
 }));
